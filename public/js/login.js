b82ff0b1d902c3154223980070842630328f59b9
Wesley Berry <wesleyberry52@gmail.com>
2019-04-13 13:30:30 -0500

    Added front-end JS for signin

---
+++ public/js/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $(".sign-in-form").on("submit", function () {
+        event.preventDefault();
+        var username = $("input#username-signin");
+        var password = $("input#password-signin");
+        var role = $("input[name='role-signin']:checked");
+        var signBody = {
+            username: username,
+            password: password,
+            role: role
+        };
+        if (!username || !password || !role) {
+            alert("Please fill out every field");
+        } else {
+            $.ajax("/api/login", {
+                data: signBody
+            }).then(function (response) {
+                window.location.replace(response);
+            }).catch(function (err) {
+                console.log(err);
+            });
+        }
+    });
+});
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
     $(".create-form").on("submit", function (event) {
         event.preventDefault();
-        var checked = $("input[name='role']:checked").val();
-        var user = $("#userReg").val().trim();
-        var pass = $("#passReg").val().trim();
-        var confirm = $("#confirmReg").val().trim();
-        var email = $("#emailReg").val().trim();
+        var role = $("input[name='role-signup']:checked").val();
+        var user = $("#user-reg").val().trim();
+        var pass = $("#pass-reg").val().trim();
+        var confirm = $("#confirm-reg").val().trim();
+        var email = $("#email-reg").val().trim();
         var newMember = {
             username: user,
             password: pass,
             email: email,
-            role: checked
+            role: role
         };
         if (pass !== confirm) {
             alert("Your passwords do not match");
-        } else if (!newMember.pass || !newMember.user || !newMember.confirm || !newMember.email) {
+        } else if (!pass || !user || !confirm || !email || !role) {
             alert("Please fill out every field");
         } else {
             $("form :input").val("");
             $.post("/api/signup", {
                 data: newMember
-            }).then(function (data) {
+            }).then(function (response) {
                 console.log("Signup.js test: Data returned to client for registration.");
             }).catch(handleSignupError);
         }
