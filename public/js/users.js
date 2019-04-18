faf6b841209198ebfd6f0a8ad6107ee533b1f796
Wesley Berry <wesleyberry52@gmail.com>
2019-04-18 14:55:51 -0500

    Can populate the Venue page with gig and venues information

--- public/js/users.js
+++ public/js/users.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     $.get("/api/gigs").then(function(data) {
+        location.reload();
         console.log(data);
     });
 });
--- routes/gig-routes.js
+++ routes/gig-routes.js
@@ -4,17 +4,27 @@ var db = require("../models");
 module.exports = function(app) {
     //  Posts to gigs table from venue view
     app.post("/api/gigs", function(req, res) {
-        db.Gig.create({
-            date: req.body.gigDate,
-            description: req.body.gigDes,
-            genre: req.body.gigGenre,
-            VenueId: req.user.id
+        db.Venue.findAll({
+            where: {
+                UserId: req.user.id
+            }
+        }).then(function(venueInfo) {
+            console.log(venueInfo[0].image);
+            console.log(venueInfo[0].venueName);
+                db.Gig.create({
+                    date: req.body.gigDate,
+                    description: req.body.gigDes,
+                    genre: req.body.gigGenre,
+                    VenueId: req.user.id,
+                    gigName: venueInfo[0].image,
+                    profileImage: venueInfo[0].venueName
         }).then(function() {
             res.send(200);
         }).catch(function(err) {
             console.log(err);
             res.json(err);
-        })
+        });
+        });
     });
     // Get one specific gig
     app.get("/api/gigs/:gigId", function (req, res) {
@@ -43,12 +53,11 @@ module.exports = function(app) {
                     UserId: req.user.id
                 }
             }).then(function(dbVenue) {
+                console.log(dbGig);
                 var hbsObject = {
-                    gigs: dbGig,
-                    name: dbVenue[0].venueName,
-                    image: dbVenue[0].image
+                    gig: dbGig
                 }   
-                res.send(hbsObject);
+                res.send(dbGig);
                 console.log(hbsObject);
                 console.log(dbVenue[0].venueName);
                 console.log(dbVenue[0].image);
