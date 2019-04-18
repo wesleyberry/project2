6752c22262e1660eeb2218f563401403b18e971d
lqdsqd <wesley.lebert@gmail.com>
2019-04-18 09:43:01 -0500

    venue view built

--- config/passport.js
+++ config/passport.js
@@ -3,39 +3,36 @@ var LocalStrategy = require("passport-local").Strategy;
 
 var db = require("../models");
 
-passport.use(new LocalStrategy(
-    {
-      usernameField: "name"
-    },
-    function(name, password, done) {
-      // console.log("are we looking for something")
-      db.User.findOne({
-        where: {
-          name: name
-        }
-      }).then(function(dbUser) {
-        if (!dbUser) {
-          return done(null, false, {
-            message: "Incorrect username."
-          });
-        }
-        else if (!dbUser.validPassword(password)) {
-          return done(null, false, {
-            message: "Incorrect password."
-          });
-        }
-        // console.log("found em")
-        return done(null, dbUser);
-      });
-    }
-  ));
-  passport.serializeUser(function(user, cb) {
-    cb(null, user);
-  });
-  
-  passport.deserializeUser(function(obj, cb) {
-    cb(null, obj);
-  });
-  
-  module.exports = passport;
-  
+passport.use(new LocalStrategy({
+    passwordField: "password"
+  },
+  function (name, password, done) {
+    // console.log("are we looking for something")
+    db.User.findOne({
+      where: {
+        name: name
+      }
+    }).then(function (dbUser) {
+      if (!dbUser) {
+        return done(null, false, {
+          message: "Incorrect username."
+        });
+      } else if (!dbUser.validPassword(password)) {
+        return done(null, false, {
+          message: "Incorrect password."
+        });
+      }
+      // console.log("found em")
+      return done(null, dbUser);
+    });
+  }
+));
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
+module.exports = passport;
