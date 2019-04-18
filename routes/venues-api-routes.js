98194a8be7f7d7829d8b8bfa80f6456e4e6d2189
Ken Riley <kriley314@gmail.com>
2019-04-18 09:46:34 -0500

    Updated venue-api-routes.js processing.

--- routes/venues-api-routes.js
+++ routes/venues-api-routes.js
@@ -1,42 +1,18 @@
 var db = require("../models");
 
 module.exports = function (app) {
-    // Get route for retrieving a single post
+    // Get route for retrieving a single venue
     app.get("/api/venues/", function (req, res) {
-        // Here we add an "include" property to our options in our findOne query
-        // We set the value to an array of the models we want to include in a left outer join
-        // In this case, just db.Venue
-        db.Gig.findAll({})
-            .then(function (dbGig) {
-                db.Venue.findOne({
-                    where: {
-                        UserId: req.user.id
-                    }
-                }).then(function (dbVenue) {
-                    // console.log(dbArtist);
-                    res.json({venue: dbVenue, gig: dbGig })
-                })
-                //res.json(dbGig);
-            });
-
-        
-
-
+        db.Venue.findOne({
+            where: {
+                UserId: req.user.id
+            }
+        }).then(function (dbVenue) {
+            var hbObject = {
+                venue: dbVenue
+            }
+            // console.log(dbVenue);
+            res.render("index-venue", hbObject);
+        });
     });
-
-    // // Get route for retrieving a single Artist
-    // app.get("/api/artists/", function (req, res) {
-    //     // Here we add an "include" property to our options in our findOne query
-    //     // We set the value to an array of the models we want to include in a left outer join
-    //     // In this case, just db.Artists
-    //     db.Artist.findAll({
-
-    //     }).then(function (dbArtist) {
-
-    //         res.json(dbArtist);
-    //         // res.send(dbArtist);
-
-    //     });
-    // });
-
 }
